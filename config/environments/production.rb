eea5c6fa3410c15138944e7f91cd46747833437f
Michael Prins <michael.prins@me.com>
2018-05-02 19:33:38 +0200

    Send in Blue SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Email configuration
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'route62.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp-relay.sendinblue.com',
+    port: 587,
+    user_name: 'route62rally@gmail.com',
+    password: 'fILOUXAPV4MgTvrh'
+  }
 end
