d84832c6a2a4331a797f4505e5ca399ad4106525
Michael Prins <michael.prins@me.com>
2018-05-31 09:20:58 +0200

    Fix mailer recipients

--- app/mailers/contacts_mailer.rb
+++ app/mailers/contacts_mailer.rb
@@ -7,7 +7,7 @@ class ContactsMailer < ApplicationMailer
     mail(
       to: ['brucedt@iafrica.com', 'ammi@iafrica.com', 'route62rally@gmail.com'],
       subject: "Route 62 Rally Contact Form Query",
-      reply_to: %("#{contact.name} <#{contact.email}>")
+      reply_to: "#{contact.name} <#{contact.email}>"
     )
   end
 end
--- app/mailers/registrations_mailer.rb
+++ app/mailers/registrations_mailer.rb
@@ -7,7 +7,7 @@ class RegistrationsMailer < ApplicationMailer
     mail(
       to: registration.email,
       subject: "Route 62 Rally #{registration.year} Registration (#{registration.reference})",
-      reply_to: %("Route 62 Rally <route62rally@gmail.com>"),
+      reply_to: 'Route 62 Rally <route62rally@gmail.com>',
       bcc: ['brucedt@iafrica.com', 'ammi@iafrica.com', 'route62rally@gmail.com']
     )
   end
